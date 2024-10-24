0a8ad325048e103b685db0013a0e7fc1ed6394eb
Erik <erik.agayan1@gmail.com>
2024-10-24 14:40:14 +0300

    Add comments

--- posts/models.py
+++ posts/models.py
@@ -13,3 +13,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,34 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(
+        source="author.username"
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["id", "post", "author", "content", "created_at"]
+        read_only_fields = ["author", "created_at"]
+
+
+class CommentSerializerForPost(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(
+        source="author.username"
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["author", "content", "created_at"]
+        read_only_fields = ["author", "created_at"]
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source="author.username")
+    comments = CommentSerializerForPost(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ["id", "title", "content", "author", "created_at", "updated_at"]
+        fields = ["id", "title", "content", "author", "created_at", "updated_at", "comments"]
         read_only_fields = ["author", "created_at", "updated_at"]
